f7b81316bb7cf477ea1a4afde97692ca4acd6534
Dominique Jeldes <1121623@est.intec.edu.do>
2025-01-10 18:21:32 -0400

    update 9

--- GestorInventario/GestorInventario/Vistas/CategoriasForm.Designer.cs
+++ GestorInventario/GestorInventario/Vistas/CategoriasForm.Designer.cs
@@ -29,21 +29,72 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CategoriasForm));
+            this.btnAgregarCat = new System.Windows.Forms.Button();
+            this.btnEditarCat = new System.Windows.Forms.Button();
+            this.btnEliminarCat = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnAgregarCat
+            // 
+            this.btnAgregarCat.Location = new System.Drawing.Point(347, 231);
+            this.btnAgregarCat.Name = "btnAgregarCat";
+            this.btnAgregarCat.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregarCat.TabIndex = 0;
+            this.btnAgregarCat.Text = "Agregar";
+            this.btnAgregarCat.UseVisualStyleBackColor = true;
+            // 
+            // btnEditarCat
+            // 
+            this.btnEditarCat.Location = new System.Drawing.Point(347, 275);
+            this.btnEditarCat.Name = "btnEditarCat";
+            this.btnEditarCat.Size = new System.Drawing.Size(75, 23);
+            this.btnEditarCat.TabIndex = 1;
+            this.btnEditarCat.Text = "Editar";
+            this.btnEditarCat.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarCat
+            // 
+            this.btnEliminarCat.Location = new System.Drawing.Point(347, 322);
+            this.btnEliminarCat.Name = "btnEliminarCat";
+            this.btnEliminarCat.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarCat.TabIndex = 2;
+            this.btnEliminarCat.Text = "Eliminar";
+            this.btnEliminarCat.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(299, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(177, 168);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // CategoriasForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightBlue;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(482, 391);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnEliminarCat);
+            this.Controls.Add(this.btnEditarCat);
+            this.Controls.Add(this.btnAgregarCat);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CategoriasForm";
             this.Text = "Categorias";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAgregarCat;
+        private System.Windows.Forms.Button btnEditarCat;
+        private System.Windows.Forms.Button btnEliminarCat;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
