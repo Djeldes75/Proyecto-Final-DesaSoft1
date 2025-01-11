1f1268f38a6c4b776d3d2425dae5caf34819f63b
Dominique Jeldes <1121623@est.intec.edu.do>
2025-01-10 20:36:24 -0400

    update 10

--- GestorInventario/GestorInventario/Data/ConexionDB.cs
+++ GestorInventario/GestorInventario/Data/ConexionDB.cs
@@ -5,7 +5,6 @@ namespace SistemaInventario.Modelos
     public static class ConexionBD
     {
         private static string cadenaConexion = "Data Source=NEWVOID\\GESTORINVENTARIO;Initial Catalog=GestorInventario;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
-
         public static SqlConnection ObtenerConexion()
         {
             return new SqlConnection(cadenaConexion);
